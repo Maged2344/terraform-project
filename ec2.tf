b33301c053ad306a854ee5c99a87651ee0a7cd25
Maged2344 <maged3bdelfatah@gmail.com>
2022-09-25 15:52:50 +0200

    ec2 modifed

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami             = "ami-026b57f3c383c2eec"
   instance_type   = "t2.micro"
   subnet_id       = module.iti_network.aws_subnet_public1
-  security_groups = [module.iti_network.aws_security_group1]
+  vpc_security_group_ids = [module.iti_network.aws_security_group1]
   provisioner "local-exec" {
     command = "echo ${self.public_ip} >> bastion.ip.txt"
   }
@@ -15,7 +15,7 @@ resource "aws_instance" "application" {
   ami             = "ami-026b57f3c383c2eec"
   instance_type   = "t2.micro"
   subnet_id       = module.iti_network.aws_subnet_private1
-  security_groups = [module.iti_network.aws_security_group2]
+  vpc_security_group_ids = [module.iti_network.aws_security_group2]
   tags = {
     Name = "application"
   }
